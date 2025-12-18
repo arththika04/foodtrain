725ef484ea41a36d2cbee1265e3efc5e2fa46b25
Srikantha_Arththika <arththika04@gmail.com>
2025-12-18 13:21:09 +0530

    add

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,26 @@
 export default function Login() {
   return (
     <div className="login-page">
-      <h1 className="fire-text">TRAIN MEAL</h1>
-
-      <div className="login-box">
+      <div className="login-card">
         <h2>Login</h2>
-        <input placeholder="Enter Mobile Number" />
-        <input type="password" placeholder="Password" />
+
+        <div className="input-box">
+          <input placeholder="Username" />
+        </div>
+
+        <div className="input-box">
+          <input type="password" placeholder="Password" />
+        </div>
+
+        <div className="options">
+          <label><input type="checkbox" /> Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+
         <button>Login</button>
+        <p className="register">
+          Don't have an account? <a href="#">Register</a>
+        </p>
       </div>
     </div>
   );
