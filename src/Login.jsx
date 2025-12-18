519466f54b833600df2066d0ccc6a8925182fc2d
Srikantha_Arththika <arththika04@gmail.com>
2025-12-18 13:10:55 +0530

    add

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,14 @@
-function Login() {
+export default function Login() {
   return (
-    <div className="login-box">
-      <h2>Login</h2>
-      <input placeholder="Mobile Number" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
+    <div className="login-page">
+      <h1 className="fire-text">TRAIN MEAL</h1>
+
+      <div className="login-box">
+        <h2>Login</h2>
+        <input placeholder="Enter Mobile Number" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
     </div>
   );
 }
-
-export default Login;
